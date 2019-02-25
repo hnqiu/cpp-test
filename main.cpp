cb9a5a2c0213301b14ce745aa526ef4b7751d0c6
hnqiu <huanneng.qiu@gmail.com>
2019-02-25 12:30:50 +1100

    Add function feature tests

--- main.cpp
+++ main.cpp
@@ -7,11 +7,15 @@
 #include "string_test.h"
 #include "sizeof_test.h"
 #include "continue_test.h"
+#include "return_list_test.h"
+#include "return_pointer_to_array_test.h"
 
 int main(int argc, char *argv[]) {
     //string_test();
     //sizeof_test();
-    continue_test();
+    //continue_test();
+    //return_list_test();
+    return_pointer_to_array_test();
     
     return 0;
 }
