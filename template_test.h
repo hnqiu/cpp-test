82005e51608ab36c4c5cb90eeb7e35b363d5eb1f
hnqiu <huanneng.qiu@gmail.com>
2019-03-15 17:42:31 +1100

    Updating template test TBC

---
+++ template_test.h
@@ -0,0 +1,20 @@
+/* Copyright (C) 2019 hnqiu. All rights reserved.
+ * Licensed under the MIT License. See LICENSE for details.
+ */
+
+
+#pragma once
+
+#include "inheritance_test.h"
+
+template <typename T>
+class Network
+{
+private:
+    /* data */
+public:
+    Network(/* args */);
+    ~Network();
+};
+
+
